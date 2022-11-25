a5593bf488c43fb39b245bd90b96519fea071b63
Tony Lockhart <tony.lockhart@ymail.com>
2022-11-25 17:46:48 -0500

    feat: Added recursive binary search

---
+++ chapter1/algorithm/binarySearch/recursive/README.md
@@ -0,0 +1,6 @@
+<h1 align="centery">Binary Search: Typescript Algorithm</h1>
+<p align="center"><kbd><img src ="../flowcharts/BinarySearchFlowChart.png"/></kbd></p><br/>
+
+# Compile: tsc app.ts -w
+# Execute: npm start
+# Open the console
---
+++ chapter1/algorithm/binarySearch/recursive/app.js
@@ -0,0 +1,40 @@
+"use strict";
+function getMid(low, high) {
+    /***
+     * If the sum of the indexes is odd, you must round
+     *  down to the floor, since there will be a remainder
+     ***/
+    const mean = Math.floor((low + high) / 2);
+    // protect against overflow
+    //   const mean = low +((low + high) / 2);
+    return mean;
+}
+const binarySearch = (items, item, low, high) => {
+    // defaults:
+    // let index = low;
+    let lowIndex = low;
+    let highIndex = high;
+    // Base Condition:
+    // highIndex <=0 means length of array can be 0 or 1, -1 woul make it -1 or 0
+    if (highIndex <= 0 || lowIndex >= highIndex)
+        return null;
+    let mid = getMid(lowIndex, highIndex);
+    console.log("Mid:", mid, ";item:", item, items[mid], item === items[mid]);
+    if (items[mid] === item) {
+        console.log("Returning mid:", mid);
+        return mid;
+    }
+    else if (items[mid] > item) {
+        highIndex = mid - 1;
+    }
+    else if (items[mid] < item) {
+        lowIndex = mid + 1;
+    }
+    console.log("lowIndex:", lowIndex, "; highIndex:", highIndex);
+    return binarySearch(items, item, lowIndex, highIndex);
+};
+const items = [0, 35, 36, 41, 46, 59, 91, 100];
+const item = 91;
+const low = 0;
+const high = items.length - 1;
+console.log("What is the index of the item?", binarySearch(items, item, low, high));
